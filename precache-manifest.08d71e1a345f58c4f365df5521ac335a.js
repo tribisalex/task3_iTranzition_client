39a0581c764a619dd6420cde154070a09cc58aaf
Alexandr <tribisalexandr@yandex.ru>
2022-02-18 00:05:53 +0300

    Updates

---
+++ precache-manifest.08d71e1a345f58c4f365df5521ac335a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ce00c292fdd68826636c9a605f5a0e",
+    "url": "/task3_iTranzition_client/index.html"
+  },
+  {
+    "revision": "35c8389e9f02fb571eba",
+    "url": "/task3_iTranzition_client/static/js/2.2eb238be.chunk.js"
+  },
+  {
+    "revision": "17fd62de34a636d87042",
+    "url": "/task3_iTranzition_client/static/js/main.3e7031f5.chunk.js"
+  },
+  {
+    "revision": "e80da9c4b71c6beb98fa",
+    "url": "/task3_iTranzition_client/static/js/runtime~main.dcbb2195.js"
+  }
+]);
